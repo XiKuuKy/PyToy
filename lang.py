2d591b49a1cd638384c43a597d3f346df99278d0
spharms <simon@phillipharms.com>
2015-01-21 09:30:07 +0800

    Added more docs

--- lang.py
+++ lang.py
@@ -29,8 +29,25 @@ class PyToy:
         """
         call(cmd,shell=True)
     def set_shell_color(self,color):
+        """
+        set_shell_color(color):
+            set the color of the entire shell
+        example:
+            toy.set_shell_color('red')
+        :param color:
+        :return:
+        """
         call("color "+color,shell=True)
     def printc(self,s,c):
+        """
+        printc:
+            colored print()
+        Example:
+            toy.printc("Hello, World!",'green')
+        :param s:
+        :param c:
+        :return:
+        """
         if c == 'green':
             print(Fore.GREEN + s)
         if c == 'red':
@@ -40,15 +57,61 @@ class PyToy:
         if c == 'purple':
             print(Fore.PURPLE + s)
     def ipy(self):
+        """
+        ipy():
+            start an IPython shell
+        Example:
+            toy.ipy()
+        :return:
+        """
         self.ipshell = InteractiveShellEmbed()
         self.ipshell()
     def stdin(self):
+        """
+        stdin():
+            waits for input
+        Example:
+            print('Hello')
+            toy.stdin()
+        :return:
+        """
         input('...')
     def printf(self,s):
+        """
+        printf(string):
+            print with no new line
+        Example:
+            toy.printf("Hi")
+        :param s:
+        :return:
+        """
         print(s,end="")
     def prints(self,s):
+        """
+        prints(string):
+            prints a whitespace after the string
+        Example:
+            toy.prints("There will be a space after this")
+        :param s:
+        :return:
+        """
         print(s,end=" ")
     def printt(self,s):
+        """
+        printt(string):
+            print with a ending tab.
+        Example:
+            toy.printt("Look out incoming tab!")
+        :param s:
+        :return:
+        """
         print(s,end="\t")
     def newline(self):
+        """
+        newline():
+            prints a newline.
+        Example:
+            newline()
+        :return:
+        """
         print("\n")
